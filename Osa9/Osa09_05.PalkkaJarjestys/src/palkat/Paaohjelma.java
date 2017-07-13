dec5531ef6bd8feb192ea410555bac7e0e366849
Vahtix <vahtis91@gmail.com>
2017-07-13 13:44:59 +0300

    CompareTo method

---
+++ Osa9/Osa09_05.PalkkaJarjestys/src/palkat/Paaohjelma.java
@@ -0,0 +1,21 @@
+package palkat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        ArrayList<Ihminen> ihmisia = new ArrayList<>();
+        ihmisia.add(new Ihminen("Matti", 150000));
+        ihmisia.add(new Ihminen("Merja", 500));
+        ihmisia.add(new Ihminen("Pertti", 80));
+
+        System.out.println(ihmisia);
+        
+        Collections.sort(ihmisia);
+        
+        System.out.println(ihmisia);
+
+    }
+}
